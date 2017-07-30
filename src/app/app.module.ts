02eaddac3bc674bf35054fbf5b7b3cc311740c97
Luis Manuel <luisman40@gmail.com>
2017-07-30 18:05:34 +0200

    Added adsense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { ConversionComponent } from './conversion/conversion.component';
 import { ConversionService } from './conversion/conversion.service';
+import { AdsenseModule } from 'ng2-adsense'
 
 @NgModule({
   declarations: [
@@ -19,7 +20,12 @@ import { ConversionService } from './conversion/conversion.service';
     FormsModule,
     HttpModule,
     JsonpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    AdsenseModule.forRoot({
+      adClient: 'ca-pub-4672802874016023',
+      // adSlot: 7259870550
+    }),
+
   ],
   providers: [ConversionService],
   bootstrap: [AppComponent]
