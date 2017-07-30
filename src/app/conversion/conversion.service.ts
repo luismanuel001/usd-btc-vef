70c2a47b4345ba90b29889ec3a5b18bd09ba1a53
Luis Manuel <luisman40@gmail.com>
2017-07-30 17:39:36 +0200

    Changed btc rate urls to localbitcoin ave_6h rates

--- src/app/conversion/conversion.service.ts
+++ src/app/conversion/conversion.service.ts
@@ -7,21 +7,21 @@ export class ConversionService {
 
   private btcPriceInUsd = 900;
   private btcPriceInVef = 2700000;
-  private refreshInterval = 5 * 1000; // In seconds 
-  private btcPriceInUsdUrl = 'https://api.gdax.com/products/BTC-USD/ticker';
-  private btcPriceInVefUrl = 'https://api.blinktrade.com/api/v1/VEF/ticker?callback=JSONP_CALLBACK';
+  private refreshInterval = 5 * 1000; // In seconds
+  private btcPriceInUsdUrl = 'https://cors-anywhere.herokuapp.com/localbitcoins.com/bitcoinaverage/ticker-all-currencies/';
+  private btcPriceInVefUrl = 'https://cors-anywhere.herokuapp.com/localbitcoins.com/bitcoinaverage/ticker-all-currencies/';
 
   constructor(private http: Http, private jsonp: Jsonp) { }
 
   getBtcPriceInUsd(): Observable<number> {
     return this.http.get(this.btcPriceInUsdUrl)
-      .map((res: Response) => res.json().price)
+      .map((res: Response) => res.json().USD['avg_6h'])
       .catch(err => Observable.throw(err || 'Error getting btcPriceInUsd'));
   }
 
   getBtcPriceInVef(): Observable<number> {
-    return this.jsonp.get(this.btcPriceInVefUrl)
-      .map((res: Response) => res.json().sell)
+    return this.http.get(this.btcPriceInVefUrl)
+      .map((res: Response) => res.json().VEF['avg_6h'])
       .catch(err => Observable.throw(err || 'Error getting btcPriceInVef'));
   }
 
